e293cc37d8fa96290f8e2bc553c09388e92de3fd
Deepak  Joshi <dj753233@gmail.com>
2024-08-10 22:08:00 +0530

    Create Number of Islands.cpp

---
+++ Number of Islands.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid, int r, int c) {
+        if (r < 0 || c < 0 || r >= grid.size() || c >= grid[0].size() || grid[r][c] != '1') {
+            return;
+        }
+
+        
+        grid[r][c] = '0';
+
+        
+        dfs(grid, r + 1, c); // down
+        dfs(grid, r - 1, c); // up
+        dfs(grid, r, c + 1); // right
+        dfs(grid, r, c - 1); // left
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] == '1') { 
+                    ++count;
+                    dfs(grid, i, j); 
+                }
+            }
+        }
+        return count;
+    }
+};
